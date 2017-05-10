9b688fc9ee0f9e1122d4dd5719b01c8379405940
Qendorian <vrieling_k@live.nl>
2017-05-10 13:32:38 +0200

    Buttons class now works?

--- source/Buttons.hx
+++ source/Buttons.hx
@@ -1,15 +1,44 @@
 package;
-
+import openfl.display.Sprite;
+import openfl.Assets;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import openfl.events.MouseEvent;
 /**
  * ...
  * @author Olav
  */
 class Buttons 
 {
-
-	public function new() 
+	var imageName:String;
+	var image:Bitmap;
+	
+	public function new(_imageName:String) 
+	{
+		super();
+		imageName = _imageName;
+		var imageData = Assets.getBitmapData('img/buttons/${imageName}.png');
+		var image = new Bitmap(imageData);
+		addChild(image);
+		
+		addEventListener(MouseEvent.CLICK, OnMouseClick);
+		addEventListener(MouseEvent.ROLL_OVER, onRoll);
+		addEventListener(MouseEvent.ROLL_OUT, onOut);
+	}
+	
+	//adds mouse click functionality to image to create button
+	function OnMouseClick(event:MouseEvent)
 	{
 		
 	}
 	
+	public function onRoll(event: MouseEvent)
+	{
+		
+	}
+	
+	public function onOut(event: MouseEvent)
+	{
+		
+	}
 }
