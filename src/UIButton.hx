1a427fc16a1b757bbbe80e97d65827efa5697d0d
OlavDeng2 <olavkruithof@gmail.com>
2017-05-11 21:31:57 +0200

    added button class

--- src/UIButton.hx
+++ src/UIButton.hx
@@ -1,13 +1,39 @@
 package;
 
+
+import openfl.display.Sprite;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.Assets;
+import openfl.events.MouseEvent;
 /**
  * ...
  * @author Olav
  */
-class UIButton 
+class UIButton extends Sprite
 {
 
-	public function new() 
+	public function new(buttonName: String) 
+	{
+		super();
+		//Get the data for the image to render
+		var startButtonImageData = Assets.getBitmapData('img/UIElements/${buttonName}.png');
+		var startButtonImage = new Bitmap(startButtonImageData);
+		
+		//set the transformation point of the image at its center
+		startButtonImage.x = -startButtonImage.width / 2;
+		startButtonImage.y = -startButtonImage.height / 2;
+		
+		//add the image
+		addChild(startButtonImage);
+		
+		//event listener so that clicking stuff actually works
+		addEventListener(MouseEvent.CLICK, OnMouseClick);
+
+	}
+	
+		
+	function OnMouseClick(event:MouseEvent)
 	{
 		
 	}
