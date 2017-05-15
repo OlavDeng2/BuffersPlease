8b56e4ee9138d11b45c411b875c99aabb8e21ef0
Qendorian <vrieling_k@live.nl>
2017-05-15 21:09:47 +0200

    Putting in the var and functions for displaying

--- bin/windows/neko/release/haxe/ApplicationMain.hx
+++ bin/windows/neko/release/haxe/ApplicationMain.hx
@@ -24,7 +24,7 @@ import haxe.macro.Expr;
 		
 		var config = {
 			
-			build: "110",
+			build: "111",
 			company: "Olav",
 			file: "BuffersPlease",
 			fps: 60,
--- src/StageMainGame.hx
+++ src/StageMainGame.hx
@@ -8,20 +8,56 @@ import openfl.Assets;
  * ...
  * @author Olav
  */
-class StageMainGame 
+class StageMainGame extends Sprite
 {
 	//Declare some necesarry variables
 	var shackBackData:BitmapData = Assets.getBitmapData("shackUI/Background.png");
 	var shackBack:Bitmap = new Bitmap = shackBackData;
+	var beakerData:BitmapData = Assets.getBitmapData("shackUI/Beaker.png");
+	var beaker:Bitmap = new Bitmap = beakerData;
+	var bellData:BitmapData = Assets.getBitmapData("shackUI/Bell.png");
+	var bell:Bitmap = new Bitmap = bellData;
+	var chemGuideData:BitmapData = Assets.getBitmapData("shackUI/Chemguide.png");
+	var chemGuide:Bitmap = new Bitmap = chemGuideData;
+	var clientData:BitmapData = Assets.getBitmapData("shackUI/Client.png");
+	var client:Bitmap = new Bitmap = clientData;
+	var drawerCloseData:BitmapData = Assets.getBitmapData("shackUI/CupboardClosed.png");
+	var drawerClose:Bitmap = new Bitmap = drawerCloseData;
+	var drawerOpenData:BitmapData = Assets.getBitmapData("shackUI/CupboardOpened.png");
+	var drawerOpen:Bitmap = new Bitmap = drawerOpenData;
+	var deskData:BitmapData = Assets.getBitmapData("shackUI/Desk.png");
+	var desk:Bitmap = new Bitmap = deskData;
+	var meterData:BitmapData = Assets.getBitmapData("shackUI/Meter.png");
+	var meter:Bitmap = new Bitmap = meterData;
+	var notepadData:BitmapData = Assets.getBitmapData("shackUI/Notepad.png");
+	var notepad:Bitmap = new Bitmap = notepadData;
+	var screenData:BitmapData = Assets.getBitmapData("shackUI/Screen.png");
+	var screen:Bitmap = new Bitmap = screenData;
 	//do the necesarry setups
 	
 	public function new() 
 	{
-		
+		addShackBack();
 	}
 	
 	static function addShackBack()
 	{
 		addChild(shackBack);
 	}
+	
+	static function addBeaker()
+	{
+		addChild(beaker);
+	}
+	
+	static function addBell()
+	{
+		addChild(bell);
+	}
+	
+	static function addChemguide()
+	{
+		addChild(chemGuide);
+	}
+	
 }
